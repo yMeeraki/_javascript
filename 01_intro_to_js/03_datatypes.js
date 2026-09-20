f41b67633d2a9b820ae0b053a28a72f2132e9af9
Yukti Gupta <yuktigupta95@gmail.com>
2026-09-21 00:28:08 +0530

    Add initial implementation of data types module with examples and explanations

---
+++ 01_intro_to_js/03_datatypes.js
@@ -0,0 +1,33 @@
+/*
+A JavaScript function is a reusable block of code designed to perform a specific task. 
+log() - to print in console
+typeof() - tell the datatype
+*/
+
+/*
+Data Types
+
+A variable can store different kinds of information.
+
+The three important types for this lesson are:
+*/
+
+// String - Text
+let firstName = "Priya";
+console.log("Name", firstName);
+console.log(typeof firstName);
+
+// Numbers
+let age = 13;
+console.log("Age", age);
+console.log(typeof age);
+
+// Boolean - true or false
+let isStudent = true;
+console.log("Is he/she is a student?", isStudent);
+console.log(typeof isStudent);
+
+
+// --------------------------------
+let number = "49"
+console.log(typeof(number));
